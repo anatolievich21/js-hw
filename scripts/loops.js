35d342d69bbe7630bde03b0aa81fb1a2cf845b1d
anatolievich_21 <pastushenkovladislav21@gmail.com>
2023-04-07 00:07:07 +0300

    rhomb...

--- scripts/loops.js
+++ scripts/loops.js
@@ -124,16 +124,16 @@ const numbers = (num) =>{
 
 
 //chess
-const chess = (row, col) =>{
+const chess = (row, cell) =>{
     let result = '';
     let hex = '#';
 
     for (let i = 0; i < row; i++){
-        if (col % 2 === 0) {
+        if (cell % 2 === 0) {
             hex = (hex === '#') ? '.' : '#';
         };
 
-        for (let j = 0; j < col; j++) {
+        for (let j = 0; j < cell; j++) {
             result += hex;
             hex = (hex === '#') ? '.' : '#';
         };
@@ -143,3 +143,116 @@ const chess = (row, col) =>{
 
     console.log(result);
 }
+
+
+
+//cubes
+const cubes = () => {
+    const num = +prompt('Введіть кількість елементів в масиві:');
+    const arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push(i ** 3);
+    }
+
+    console.log(arr);
+}
+
+
+
+//multiply table
+const multiplyTable = () => {
+    const arr = [];
+
+    for (let i = 0; i < 10; i++){
+        arr[i] = [];
+
+        for (let j = 0; j < 10; j++) {
+            arr[i][j] = (i + 1) * (j + 1);
+        }
+    }
+
+    console.log(arr)
+}
+
+
+
+//read array of objects
+const readArrayOfObjects = () => {
+    const arr = [];
+
+    while (true){
+        const obj = {};
+
+        while (true){
+            const key = prompt(`Введіть ключ:`);
+
+            if(!key){
+                break;
+            }
+
+            const value = prompt(`Введіть значення:`);
+            obj[key] = value;
+        }
+
+        arr.push(obj);
+
+        const more = confirm("Хочете продовжити введення об'єктів?");
+
+        if (!more){
+            break;
+        }
+    }
+
+    return arr;
+}
+
+
+
+//rhomb
+
+
+
+//DOM: multiply table
+const multiplyTableDOM = () => {
+    const table = document.createElement('table');
+
+    for (let i = 0; i < 10; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 0; j < 10; j++) {
+            const cell = document.createElement('td');
+            const result = (i + 1) * (j + 1);
+
+            cell.style.cssText = `
+                padding: 10px;
+                border: 1px solid black;
+            `;
+
+            row.appendChild(cell);
+            cell.innerText = `${result}`;
+
+            cell.addEventListener('mouseover', () => {
+                for (let k = 0; k < 10; k++) {
+                    table.rows[k].cells[j].style.backgroundColor = 'darkgrey';
+                };
+
+                row.style.backgroundColor = 'darkgrey';
+                cell.style.backgroundColor = 'red';
+            });
+
+            cell.addEventListener('mouseout', () => {
+                for (let k = 0; k < 10; k++) {
+                    table.rows[k].cells[j].style.backgroundColor = '';
+                };
+
+                cell.style.backgroundColor = '';
+                row.style.backgroundColor = '';
+            });
+        }
+
+        table.appendChild(row);
+    }
+
+    document.body.appendChild(table);
+};
