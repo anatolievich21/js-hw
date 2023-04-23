f45311e45cd924eacdf967e54e3c172e36e20cc9
anatolievich_21 <pastushenkovladislav21@gmail.com>
2023-04-23 22:00:04 +0300

    hw-closures-done

--- scripts/closures.js
+++ scripts/closures.js
@@ -25,3 +25,66 @@ function makeSaver(func) {
         return result;
     };
 }
+
+
+
+//myBind
+function myBind(func, context, defaultArgs) {
+   return function (...args) {
+      const mergedArgs = [];
+      let defaultArgsIndex = 0;
+
+      for (let i = 0; i < args.length; i++) {
+
+         if (args[i] !== undefined) {
+            mergedArgs.push(args[i]);
+         } else {
+
+            if (defaultArgs[defaultArgsIndex] !== undefined) {
+               mergedArgs.push(defaultArgs[defaultArgsIndex]);
+            }
+            defaultArgsIndex++;
+         }
+      }
+
+      if (defaultArgsIndex < defaultArgs.length) {
+         mergedArgs.push(...defaultArgs.slice(defaultArgsIndex));
+      }
+
+      return func.apply(context, mergedArgs);
+   }
+}
+
+
+
+//checkResult
+function checkResult(original, validator) {
+   function wrapper(...params) {
+      let result;
+      do {
+         result = original.apply(this, params);
+      } while (!validator(result));
+      return result;
+   }
+   return wrapper;
+}
+
+const RandomHigh = checkResult(
+   () => Math.random() * 0.5 + 0.5,
+   x => typeof x === 'number' && x >= 0.5 && x <= 1
+);
+
+
+const AlwaysSayYes = checkResult(
+   () => confirm("Do you agree?"),
+   x => x === true
+);
+
+
+const respectMe = checkResult(
+   () => prompt("Please enter some text"),
+   x => typeof x === 'string' && x.trim().length > 0
+);
+const random = RandomHigh();
+const agreed = AlwaysSayYes();
+const text = respectMe();
