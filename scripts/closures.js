de15b46d2884c7c8b9027ef59819a95a8163a5b4
anatolievich_21 <pastushenkovladislav21@gmail.com>
2023-04-25 00:18:38 +0300

    hw-closures-reworking myBind

--- scripts/closures.js
+++ scripts/closures.js
@@ -30,61 +30,64 @@ function makeSaver(func) {
 
 //myBind
 function myBind(func, context, defaultArgs) {
-   return function (...args) {
-      const mergedArgs = [];
-      let defaultArgsIndex = 0;
+    return function (...args) {
+        const mergedArgs = [];
+        let defaultArgsIndex = 0;
 
-      for (let i = 0; i < args.length; i++) {
+        for (let i = 0; i < args.length; i++) {
 
-         if (args[i] !== undefined) {
-            mergedArgs.push(args[i]);
-         } else {
+            if (args[i] !== undefined) {
+                mergedArgs.push(args[i]);
+            } else {
 
-            if (defaultArgs[defaultArgsIndex] !== undefined) {
-               mergedArgs.push(defaultArgs[defaultArgsIndex]);
+                if (defaultArgs[defaultArgsIndex] !== undefined) {
+                    mergedArgs.push(defaultArgs[defaultArgsIndex]);
+                }
             }
             defaultArgsIndex++;
-         }
-      }
+        }
 
-      if (defaultArgsIndex < defaultArgs.length) {
-         mergedArgs.push(...defaultArgs.slice(defaultArgsIndex));
-      }
+        if (defaultArgsIndex < defaultArgs.length) {
+            mergedArgs.push(...defaultArgs.slice(defaultArgsIndex));
+        }
 
-      return func.apply(context, mergedArgs);
-   }
+        const filteredMergedArgs = mergedArgs.filter(item => item !== undefined);
+        // console.log(filteredMergedArgs)
+
+        return func.apply(context, filteredMergedArgs);
+    }
 }
 
 
 
 //checkResult
-function checkResult(original, validator) {
-   function wrapper(...params) {
-      let result;
-      do {
-         result = original.apply(this, params);
-      } while (!validator(result));
-      return result;
-   }
-   return wrapper;
-}
-
-const RandomHigh = checkResult(
-   () => Math.random() * 0.5 + 0.5,
-   x => typeof x === 'number' && x >= 0.5 && x <= 1
-);
-
-
-const AlwaysSayYes = checkResult(
-   () => confirm("Do you agree?"),
-   x => x === true
-);
-
-
-const respectMe = checkResult(
-   () => prompt("Please enter some text"),
-   x => typeof x === 'string' && x.trim().length > 0
-);
-const random = RandomHigh();
-const agreed = AlwaysSayYes();
-const text = respectMe();
+// function checkResult(original, validator) {
+//    function wrapper(...params) {
+//       let result;
+//       do {
+//          result = original.apply(this, params);
+//       } while (!validator(result));
+//       return result;
+//    }
+//    return wrapper;
+// }
+//
+// const RandomHigh = checkResult(
+//    () => Math.random() * 0.5 + 0.5,
+//    x => typeof x === 'number' && x >= 0.5 && x <= 1
+// );
+//
+//
+// const AlwaysSayYes = checkResult(
+//    () => confirm("Do you agree?"),
+//    x => x === true
+// );
+//
+//
+// const respectMe = checkResult(
+//    () => prompt("Please enter some text"),
+//    x => typeof x === 'string' && x.trim().length > 0
+// );
+// const random = RandomHigh();
+// const agreed = AlwaysSayYes();
+// const text = respectMe();
