252c83d5a6286b5b5cc19162bea8a473d575347d
anatolievich21 <pastushenkovladislav21@gmail.com>
2023-05-28 22:07:02 +0300

    del examples

--- script.js
+++ script.js
@@ -24,9 +24,10 @@ function createStore(reducer){
 }
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //promiseReducer
-////////////////
+/////////////////////////////////////////////////////////////
 function promiseReducer(state= {}, {type, promiseName, status, payload, error}){
     if (type === 'PROMISE'){
         return {
@@ -54,20 +55,11 @@ const actionPromise = (promiseName, promise) =>
         }
     }
 
-// const store = createStore(promiseReducer)
-
-// store.subscribe(() => console.log(store.getState())) //має запускатися 6 разів
-//
-// const delay = (ms) => new Promise((ok) => setTimeout(ok, ms));
-//
-// store.dispatch(actionPromise('delay', delay(1000)))
-// store.dispatch(actionPromise('luke', fetch("https://swapi.dev/api/people/1").then(res => res.json())))
-// store.dispatch(actionPromise('tatooine', fetch("https://swapi.dev/api/planets/1").then(res => res.json())))
 
 
-////////////////
+/////////////////////////////////////////////////////////////
 //authReducer
-////////////////
+/////////////////////////////////////////////////////////////
 function jwtDecode (token) {
     try {
         const arr = token.split('.')
@@ -109,32 +101,10 @@ const actionAuthLogin  = (token) => ({type: 'AUTH_LOGIN', token})
 const actionAuthLogout = () => ({type: 'AUTH_LOGOUT'})
 
 
-// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOiI2Mzc3ZTEzM2I3NGUxZjVmMmVjMWMxMjUiLCJsb2dpbiI6InRlc3Q1IiwiYWNsIjpbIjYzNzdlMTMzYjc0ZTFmNWYyZWMxYzEyNSIsInVzZXIiXX0sImlhdCI6MTY2ODgxMjQ1OH0.t1eQlRwkcP7v9JxUPMo3dcGKprH-uy8ujukNI7xE3A0"
-//
-// const store = createStore(authReducer)
-// store.subscribe(() => console.log(store.getState()))
-//
-// store.dispatch(actionAuthLogin(token))
-// /*{
-//     token: "eyJhbGc.....",
-//     payload: {
-//       "sub": {
-//         "id": "6377e133b74e1f5f2ec1c125",
-//         "login": "test5",
-//         "acl": [
-//           "6377e133b74e1f5f2ec1c125",
-//           "user"
-//         ]
-//       },
-//       "iat": 1668812458
-//     }
-// }*/
-// store.dispatch(actionAuthLogout()) // {}
 
-
-////////////////
+/////////////////////////////////////////////////////////////
 //cartReducer
-////////////////
+/////////////////////////////////////////////////////////////
 function cartReducer(state = {}, {type, count, good}) {
 
     if (type === 'CART_ADD') {
@@ -219,47 +189,10 @@ const actionCartSet = (good, count=1) => ({type: 'CART_SET', count, good})
 const actionCartClear = () => ({type: 'CART_CLEAR'})
 
 
-// const store = createStore(cartReducer)
-//
-// store.subscribe(() => console.log(store.getState())) //
-//
-// console.log(store.getState()) //{}
-//
-// store.dispatch(actionCartAdd({_id: 'пиво', price: 50}))
-// // {пиво: {good: {_id: 'пиво', price: 50}, count: 1}}
-// store.dispatch(actionCartAdd({_id: 'чіпси', price: 75}))
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 1},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
-// //}
-// store.dispatch(actionCartAdd({_id: 'пиво', price: 50}, 5))
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 6},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 1},
-// //}
-// store.dispatch(actionCartSet({_id: 'чіпси', price: 75}, 2))
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 6},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
-// //}
-//
-// store.dispatch(actionCartSub({_id: 'пиво', price: 50}, 4))
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 2},
-// // чіпси: {good: {_id: 'чіпси', price: 75}, count: 2},
-// //}
-//
-// store.dispatch(actionCartDel({_id: 'чіпси', price: 75}))
-// // {
-// // пиво: {good: {_id: 'пиво', price: 50}, count: 2},
-// //}
-//
-// store.dispatch(actionCartClear()) // {}
 
-
-////////////////
+/////////////////////////////////////////////////////////////
 //localStoredReducer
-////////////////
+/////////////////////////////////////////////////////////////
 function localStoredReducer(originalReducer, localStorageKey) {
     let isFirstRun = true;
 
@@ -280,9 +213,10 @@ function localStoredReducer(originalReducer, localStorageKey) {
 }
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //GraphQL requests
-////////////////
+/////////////////////////////////////////////////////////////
 shopURL = 'http://shop-roles.node.ed.asmer.org.ua/graphql';
 
 function getGQL (url = shopURL) {
@@ -318,9 +252,10 @@ function getGQL (url = shopURL) {
 gql = getGQL();
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит на перелiк кореневих категорій
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlRootCats = () =>
     gql(`query rootCats ($q: String){
     CategoryFind(query: $q){
@@ -334,9 +269,10 @@ const actionRootCats = () =>
     actionPromise('rootCats', gqlRootCats())
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит для отримання однієї категорії з товарами та картинками
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlRootCatOne = (_id) =>
     gql(`query oneCatWithGoodsImgs($q: String){
    CategoryFindOne (query: $q){
@@ -361,9 +297,10 @@ const actionRootCatOne = (_id= "6262ca7dbf8b206433f5b3d1") =>
     actionPromise('rootCatOne', gqlRootCatOne(_id));
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит на отримання товару з описом та картинками
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlGoodOne = (_id) =>
     gql(`query goodFindOne ($q: String) {
   GoodFindOne(query: $q){
@@ -383,9 +320,10 @@ const actionGoodOne = (_id = "62d3099ab74e1f5f2ec1a125") =>
     actionPromise('goodOne', gqlGoodOne(_id));
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит на реєстрацію
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlRegistration = (log, pass) =>
     gql(`mutation reg ($log: String, $pass: String){
   UserUpsert(user:{
@@ -403,9 +341,10 @@ const actionRegistration = (log, pass) =>
     actionPromise('register', gqlRegistration(log, pass));
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит на логін
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlLogin = (log, pass) =>
     gql(`query login ($log:String, $pass:String) {
   login (login: $log, password: $pass)
@@ -418,9 +357,10 @@ const actionLogin = (log, pass) =>
     actionPromise('login', gqlLogin(log, pass));
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит історії замовлень
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlHistory = () =>
     gql(`query history ($q:String) {
   OrderFind(query: $q){
@@ -436,9 +376,10 @@ const actionHistory = () =>
     actionPromise('history', gqlHistory())
 
 
-////////////////
+
+/////////////////////////////////////////////////////////////
 //Запит оформлення замовлення
-////////////////
+/////////////////////////////////////////////////////////////
 const gqlOrder = (count, _id) =>
     gql(`mutation newOrder($goods: [OrderGoodInput]) {
       OrderUpsert(order: {orderGoods: $goods}) {
@@ -453,9 +394,6 @@ const gqlOrder = (count, _id) =>
 const actionOrder = (count, _id) =>
     actionPromise('order', gqlOrder(count, _id))
 
-const store = createStore(promiseReducer);
-store.subscribe(() => console.log(store.getState()))
 
-store.dispatch(actionOrder(4, '62d3099ab74e1f5f2ec1a125'));
 
 
